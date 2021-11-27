3ba6caf5eebad35b919c68833e2a4a602b7fd337
Tal Zaloilov <tzaloilov@tractionondemand.com>
2021-11-26 18:48:23 -0800

    added audio files, tweaked key response

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+//add different keys
+
 var acceptedNotes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+function playAudio(note){
+    
+    var audioPath = "mp3 Notes/" + note + "3.mp3";
+
+    var audio = new Audio(audioPath);
+    audio.play();
+
+}
+
+
 
 function keypressHandler(e){
     // if you pressed on a real note key
     if (acceptedNotes.includes(e.key)){
         const currentBlock = document.getElementById(e.key);
-        // TODO set background color == text color
         currentBlock.classList.toggle("blockPressed");
         //switch back to regular css after 1 second
+        playAudio(e.key);
         setTimeout(()=>{
             currentBlock.classList.toggle("blockPressed");
-        }, 1000)
+        }, 200)
 
     }
 }
