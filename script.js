1b3fe62d2bc6d522a7ac1457d82c2409f6c53e9f
Tal Zaloilov <tzaloilov@tractionondemand.com>
2021-11-26 16:46:01 -0800

    changes to ui, change size on keypress

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+var acceptedNotes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
 function keypressHandler(e){
-    
+    // if you pressed on a real note key
+    if (acceptedNotes.includes(e.key)){
+        const currentBlock = document.getElementById(e.key);
+        // TODO set background color == text color
+        currentBlock.classList.toggle("blockPressed");
+        //switch back to regular css after 1 second
+        setTimeout(()=>{
+            currentBlock.classList.toggle("blockPressed");
+        }, 1000)
+
+    }
 }
 
+//add a keypress listener to the page
 window.addEventListener("keypress", keypressHandler);
